0eabd2f7b079b24ccd70bce40860227b497b90c8
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2022-04-12 20:36:38 -0700

    Spawn indicators

---
+++ Assets/Scripts/IndicatorSpawner.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndicatorSpawner : MonoBehaviour
+{
+    public GameObject indicator;
+    const int GRID_SIZE = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int x = -GRID_SIZE; x <= GRID_SIZE; x++) {
+            for (int z = -GRID_SIZE; z <= GRID_SIZE; z++) {
+                Object.Instantiate(indicator, new Vector3(x + 1, 0, z + 1), Quaternion.identity, transform);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
