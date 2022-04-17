0d4ab513881fcf6f824341026e8d3d4f2cd80281
stefxici <97343745+stefxici@users.noreply.github.com>
2022-04-16 17:50:55 -0700

    My commit

--- Assets/CubeController.cs
+++ Assets/CubeController.cs
@@ -5,7 +5,35 @@ using UnityEngine;
 public class CubeController : MonoBehaviour
 {
     void Start() { }
+//commit was implementing the: void Update(){}
+    void Update(){
+        if(Input.GetButtonDown("P1Left")){
+            Step(EventAction.Left);
+        }
+        if(Input.GetButtonDown("P1Right")){
+           Step(EventAction.Right);
+        }
+        if(Input.GetButtonDown("P1Up")){
+           Step(EventAction.Up);
+        }
+        if(Input.GetButtonDown("P1Down")){
+           Step(EventAction.Down);
+        }
+        if(Input.GetButtonDown("P2Left")){
+            Step(EventAction.Left);
+        }
+        if(Input.GetButtonDown("P2Right")){
+           Step(EventAction.Right);
+        }
+        if(Input.GetButtonDown("P2Up")){
+            Step(EventAction.Up);
+        }
+        if(Input.GetButtonDown("P2Down")){
+            Step(EventAction.Down);
+        }
 
+
+    }
     public void StepUp()
     {
         this.transform.Translate(Vector3.forward, Space.World);
